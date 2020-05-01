612a4f7b4e3e88d352c60a6ade4e73923a78c45f
Kevin Jackson <kj31428@gmail.com>
2020-04-30 22:31:05 -0700

    Updates

---
+++ precache-manifest.4d23e1d5bae290b8fe89205347a5f0e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "082cda247863292a85d63b94db0ef2fc",
+    "url": "/paletteknife-web/index.html"
+  },
+  {
+    "revision": "3c9e5ecdc12e6b6efe03",
+    "url": "/paletteknife-web/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f437ce09ee6014ae0905",
+    "url": "/paletteknife-web/static/js/2.9702a94b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/paletteknife-web/static/js/2.9702a94b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c9e5ecdc12e6b6efe03",
+    "url": "/paletteknife-web/static/js/main.56655c47.chunk.js"
+  },
+  {
+    "revision": "07da5c2954360d3d9a25",
+    "url": "/paletteknife-web/static/js/runtime-main.d5c20edd.js"
+  }
+]);
